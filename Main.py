39666675736353cf6c7dbf4c46bfc358ca68dd52
Elia <158532198+Bruss39@users.noreply.github.com>
2024-02-10 03:13:46 +0100

    Update Main.py - Add "pinned_num_method"
    Addition of a new resolution method, predating the backtracking method.

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ from math import sqrt
 from time import time
 
 # --- SETTINGS ---
-sudoku_number = 5  # 0, 1 ...
+sudoku_number = 4  # 0, 1 ...
 
 # --- PROBLEMS & SOLUTIONS ---
 boards = [
@@ -62,6 +62,24 @@ boards = [
         [0, 0, 0, 3, 0, 0, 8, 0, 6],
         [0, 0, 0, 5, 7, 0, 0, 0, 3]
     ],
+    [
+        [ 3, 13,  0,  0, 15,  0,  0,  0,  0,  7,  6,  4,  0, 16,  0, 14],
+        [ 0,  0,  0,  2, 11,  0, 13,  8,  0,  0,  0,  0,  5, 10,  0,  3],
+        [ 0,  0,  0, 14,  9,  0, 16,  4,  0,  0,  2, 15,  0,  6,  0,  0],
+        [ 0, 11,  7, 10,  0,  0,  6,  0,  9,  0,  0, 13,  0,  4, 12,  0],
+        [ 0,  2, 15,  0,  0, 16,  0,  0,  0,  6, 10,  9,  0,  0,  0,  0],
+        [ 0,  0, 16,  0,  5,  8,  0, 15, 13,  3,  0,  0, 12,  0, 11, 10],
+        [ 7,  5,  0,  0,  0,  0, 10,  0,  0,  0,  8,  1,  6, 15, 16,  0],
+        [ 1, 10,  8,  0, 13,  6,  4,  0,  0,  0,  0,  0,  0,  0,  3,  9],
+        [11, 16,  0,  0,  0,  0,  0,  0,  0, 12,  9,  7,  0,  1, 10, 15],
+        [ 0,  1, 10, 15,  7, 13,  0,  0,  0,  2,  0,  0,  0,  0,  6, 12],
+        [ 9,  3,  0,  7,  0,  0, 15,  6,  5,  0, 16,  8,  0, 11,  0,  0],
+        [ 0,  0,  0,  0,  3, 11,  8,  0,  0,  0, 15,  0,  0,  9,  7,  0],
+        [ 0,  7,  5,  0,  6,  0,  0, 12,  0,  4,  0,  0,  9,  2, 14,  0],
+        [ 0,  0,  3,  0,  4, 10,  0,  0, 16,  5,  0,  6,  1,  0,  0,  0],
+        [ 6,  0, 11, 13,  0,  0,  0,  0,  2,  9,  0, 12, 10,  0,  0,  0],
+        [12,  0,  4,  0, 16,  9,  7,  0,  0,  0,  0, 10,  0,  0,  5,  6]
+    ]
 ]
 
 
@@ -90,7 +108,7 @@ def find_empty_location(board):
     return None, None
 
 
-def solve_sudoku(board):
+def backtracking_method(board):
     """
     The function finds whether a number can be positioned within a column, a row and a square.
     If the answer is yes, it repeats the cycle for the rest of the empty cells.
@@ -115,7 +133,7 @@ def solve_sudoku(board):
             boards[sudoku_number][y][x] = num
 
             # It tries to solve the remaining part of the Sudoku by starting the cycle again.
-            if solve_sudoku(board):
+            if backtracking_method(board):
                 return True
 
             # Cancel the assignment if the previous number was incorrect.
@@ -131,12 +149,12 @@ def is_valid(board, row, col, num):
 
     Parameters:
     - board: A list of lists representing the Sudoku board.
-    - row: Row used for the validation of the number 'num'.
-    - column: Column used for the validation of the number 'num'.
+    - row: Row (x) used for the validation of the number 'num'.
+    - column: Column (y) used for the validation of the number 'num'.
     - num: Number subject to validation.
 
     Returns:
-    True if the Sudoku card was solved successfully, False otherwise.
+    The function returns True if the number is valid for the specified position, and False otherwise.
     """
     square_size = int(sqrt(len(board[0])))
 
@@ -156,15 +174,81 @@ def is_valid(board, row, col, num):
     return True
 
 
+def find_pinned_num_position(board):
+    empty_positions = []
+    for row in board:
+        for col in row:
+            if type(col) != int:
+                empty_positions.append([row.index(col), board.index(row)])  # x, y
+    return empty_positions
+
+
+def pinned_num_method(board):
+    while True:
+        old_board = [row[:] for row in board]
+
+        Cell.assign_pinned_number(board)
+        Cell.remove_already_present(board)
+
+        print("----------------------------")
+
+        if old_board == board:
+            break
+
+
+
+class Cell:
+    def __init__(self, board: list):
+        self.pinned_number = []
+        for n in range(1, len(board[0]) + 1):
+            self.pinned_number.append(n)
+
+    def __repr__(self):
+        return str(self.pinned_number)
+
+    @staticmethod
+    def assign_pinned_number(board):
+        while True:
+            x, y = find_empty_location(board)
+            if x is None:
+                break
+            board[y][x] = Cell(board)
+
+    @staticmethod
+    def remove_already_present(board):
+        empty_positions = find_pinned_num_position(board)
+        for pos in empty_positions:
+            x, y = pos
+
+            for n in range(1, len(board[0]) + 1):
+                if not is_valid(board, y, x, n):
+                    if n in board[y][x].pinned_number:
+                        board[y][x].pinned_number.remove(n)
+                        if len(board[y][x].pinned_number) == 1:
+                            board[y][x] = board[y][x].pinned_number[0]
+                            print(f"Found {board[y][x]} in x:{x}, y:{y}")
+                            break
+
+        if len(find_pinned_num_position(board)) == 0:
+            return True
+        else:
+            for pos in find_pinned_num_position(board):
+                x, y = pos
+                board[y][x] = 0
+            return False
+
+
 if __name__ == "__main__":
     sudoku_board = boards[sudoku_number]
     print("Sudoku:")
     print_board(sudoku_board)
 
     start = time()  # start stopwatch
-    if solve_sudoku(sudoku_board):
+    pinned_num_method(sudoku_board)
+
+    if backtracking_method(sudoku_board):
         timer = time() - start  # stop stopwatch
-        print(f"\nResolved  in {timer} sec:")
+        print(f"\nResolved in {timer} sec:")
         print_board(boards[sudoku_number])
     else:
         print("Sudoku has no solution.")
